acc7636a8a95210834969c65cb03bdc045284ec7
toeaung.than <toeaung.than@n2noutlook.com>
2024-07-04 00:14:06 +0700

    Add RabbitMQService Class

---
+++ RedisDemo/RabbitMQ/IRabbitMQService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RedisDemo.RabbitMQ
+{
+    public interface IRabbitMQService
+    {
+        void CloseConnection();
+        void ReceiveMessage(string queueName, Action<string> messageHandler);
+        void SendMessage(string queueName, string message);
+    }
+}
---
+++ RedisDemo/RabbitMQ/RabbitMQService.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RedisDemo.RabbitMQ
+{
+    public class RabbitMQService : IRabbitMQService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+
+        public RabbitMQService()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost", // RabbitMQ server address
+                Port = 5672,            // Default port
+                UserName = "guest",     // Default username
+                Password = "guest"      // Default password
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void CloseConnection()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+
+        public void SendMessage(string queueName, string message)
+        {
+            
+            _channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+        }
+
+        public void ReceiveMessage(string queueName, Action<string> messageHandler)
+        {
+            _channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                messageHandler.Invoke(message);
+            };
+
+            _channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+        }
+    }
+}
